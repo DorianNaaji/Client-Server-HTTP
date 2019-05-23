e17f8e90867e75075dc1bf7ee2e2201f6a6d00de
myriam <myriam.benali3@gmail.com>
2019-05-23 11:07:00 +0200

    ouverture sock

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,38 @@
 package client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client
 {
-
+		
+	public Client()
+	{
+		
+	}
+	
+	public Socket OpenSocket(String adIP, int port)
+	{
+		Socket sc=null; 
+		try 
+		{
+			
+			 sc = new Socket(InetAddress.getByName(adIP), port);
+		}
+		catch(UnknownHostException ex)
+		{
+			System.out.println("Erreur au niveau du port ");
+			return sc;
+		}
+		catch(IOException ex)
+		{
+			System.out.println("Erreur au niveau de l'ouverture du Socket");
+			return sc;
+		}
+		
+		return sc;
+		
+	}	
 }
