be8fa4f703a2b15c02852d1d40f10d449858acd0
Thiti <t.delplanque92@gmail.com>
2019-05-23 10:54:26 +0200

    début squellete methodes classe Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,33 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server
 {
+
+    public static final int defaultPort = 1026;
+
+    private ServerSocket _serverSocket;
+    //private Socket _socket; à méditer
+
+    public Server(int port) throws IOException {
+        _serverSocket = new ServerSocket(port);
+    }
+
+
+
+    public void get(){
+
+    }
+
+    public void put(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
