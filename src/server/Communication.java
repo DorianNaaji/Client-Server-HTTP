76a7c4accd768f697c6cfbf2d496a677427f9809
Thiti <t.delplanque92@gmail.com>
2019-05-26 19:40:08 +0200

    patron de la classe Communication

--- src/server/Communication.java
+++ src/server/Communication.java
@@ -2,16 +2,15 @@ package server;
 
 import com.sun.istack.internal.NotNull;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class Communication implements Runnable{
 
     private Socket _socket;
-    private OutputStream _out;
-    private InputStream _in;
+    private PrintWriter _out;
+    private BufferedReader _in;
 
     public Communication(@NotNull Socket socket){
         if (socket == null)
@@ -19,28 +18,48 @@ public class Communication implements Runnable{
         _socket = socket;
     }
 
-    private void put(){
+    private void put(String message){
 
     }
 
-    private void get(){
+    private void get(String filePath, String protocol){
+        // check protocol
+        // ouvre le fichier s'il existe
+        // ecrit le contenue du fichier dans le out
+        // flush
 
+        _out.write("coucou mon gars");
+        _out.flush();
+
+
+    }
+
+    private String interprate(String message){
+        StringTokenizer stringTokenizer = new StringTokenizer(message," ");
+        while(stringTokenizer.hasMoreElements()){
+            String element = (String)stringTokenizer.nextElement();
+            if (element.equalsIgnoreCase("GET")){
+                String filePath = (String)stringTokenizer.nextElement(); // check si prochiane ele existe
+                String protocol = (String)stringTokenizer.nextElement(); // check si prochiane ele existe
+                get(filePath,protocol);
+            }else if(element.equalsIgnoreCase("PUT")){
+
+            }else{
+
+            }
+        }
+        return null;
     }
 
     @Override
     public void run() {
         try {
-            _out = _socket.getOutputStream();
-            _in = _socket.getInputStream();
+            _out = new PrintWriter(_socket.getOutputStream());
+            _in =  new BufferedReader(new InputStreamReader(_socket.getInputStream()));
 
             while (true){
-                System.out.println(_socket.getLocalPort() + " " + _socket.getInetAddress().toString() + " " + _socket.getPort());
-                byte[] b = new byte[5000];
-                _in.read(b);
-                System.out.println(new String(b));
+                interprate(_in.readLine());
             }
-
-
 
         } catch (IOException e) {
             e.printStackTrace();
