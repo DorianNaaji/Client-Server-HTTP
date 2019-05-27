94932a82f9e99b19c926638b31bab34a39e762dd
Cécile Besson <cecile.besson@live.fr>
2019-05-27 11:50:51 +0200

    vom

--- src/server/Communication.java
+++ src/server/Communication.java
@@ -31,7 +31,7 @@ public class Communication implements Runnable{
             buildStatus(400);
         }else{
             try {
-                FileInputStream fileInputStream = new FileInputStream("/home/thiti/websites/" + filePath);
+                FileInputStream fileInputStream = new FileInputStream("C:\\www\\" + filePath);
                 buildStatus(200);
                 _out.write("\r\n");
                 readFile(fileInputStream);
